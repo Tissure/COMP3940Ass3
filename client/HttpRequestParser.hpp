4d03d477d0816ac8a69f86a0475969b84a3f5b15
Tissure <kyle.ng7512@gmail.com>
2022-11-07 12:16:47 -0800

    Create HttpRequestParser files

---
+++ client/HttpRequestParser.hpp
@@ -0,0 +1,136 @@
+//
+// Created by Admin on 2022-10-31.
+//
+
+#pragma once
+#ifndef C_WRAPPERS_HTTPSERVLETRESQUEST_HPP
+#define C_WRAPPERS_HTTPSERVLETRESQUEST_HPP
+
+#define MAX_REQUEST_SIZE 1024
+#define ASCII_TABLE_LENGTH 127
+#define METHOD_ORDER 0
+#define URL_ORDER 1
+#define LINE "\r\n"
+#define BOUNDRY "\r\n\r\n"
+#define CT "Content-Type"
+#define CT_MULTI_PART_FORM_DATA "multipart/form-data"
+#define CD "Content-Disposition:"
+
+#include <map>
+#include <string>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+
+#include "../sockets/Socket.h"
+
+using namespace std;
+
+class HttpServletRequest
+{
+public:
+    enum Method
+    {
+        GET,
+        POST,
+        DELETE
+    };
+
+    HttpServletRequest(){};
+
+    /**
+     * Parses response.
+     */
+    HttpServletRequest(Socket *socket) : socket(socket){};
+
+    HttpServletRequest::Method getMethod() { return this->method; }
+    HttpServletRequest::Method stringToMethod(string method);
+
+    /**
+     * Access map and returns copy
+     * @return
+     */
+    char *getAttribute();
+    Socket *getSocket() { return socket; };
+    void setSocket(Socket *socket) { this->socket = socket; }
+    void parse() { this->parseRequest(); }
+
+private:
+    Socket *socket;
+
+    // Head
+    Method method;
+    string version;
+    string url;
+
+    // Headers
+    map<string, string> headersMap;
+
+    // Body
+    std::vector<char> body;
+
+    // If is form type
+    map<string, string> bodyMap;
+
+    // multipart/form-data bounrdry
+    string boundry;
+
+    /**
+     * Parses request
+     * @author Ravinder Shokar
+     * @param request
+     * @return char *
+     */
+    void parseRequest();
+    void parseHead();
+    void parseHeaders();
+    void parseBody();
+    void parseMultiPart();
+    void parseMultiPartMetaData(string str, std::map<string, string> &map);
+
+    /**
+     * Returns true is pattern is found at the end of str
+     */
+    bool isMatch(string str, string pattern);
+
+    /**
+     * Returns true is pattern is in string
+     */
+    bool hasPattern(string str, string patter);
+
+    /**
+     * This will create a string of character up to the delimiter
+     * from the classes socket.
+     */
+    string getNext(string delim);
+
+    /**
+     * This will create a string of character up to the delimiter.
+     */
+    string getNext(char *str, int size, string pattern, int *cursor);
+
+    /**
+     * Returns true if key is an edge case.
+     */
+    bool headerEdgeCases(string key, string value);
+
+    /**
+     * Appends key, value to headerMap
+     */
+    void appendHeader(string key, string value);
+
+    void trim(string &str, char pattern);
+    void trim(string &str);
+
+    void parseContentDisposition(string line);
+
+    void streamIntoFile(string fileName);
+
+    /**
+     * F**g overloaded operator
+     */
+    friend ostream &operator<<(ostream &os, const HttpServletRequest &request);
+};
+
+#endif // C_WRAPPERS_HTTPSERVLETRESQUEST_HPP
